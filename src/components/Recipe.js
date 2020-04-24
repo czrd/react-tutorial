5fdaef1d1e26627fd20e5ba9f00140bb9f268b59
Takeshi Itami <tksh.itm@gmail.com>
2020-04-24 09:26:04 +0900

    update Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,33 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import Instructions from "./Instructions";
+import IngredientList from "./IngredientsList";
+
 export default function Recipe({ name, ingredients, steps }) {
   return (
     <section id={name.toLowerCase().replace(/ /g, "-")}>
       <h1>{name}</h1>
-      <ul className="ingredients">
-        {ingredients.map((ingredient, i) => (
-          <li key={i}>{ingredient.name}</li>
-        ))}
-      </ul>
-      <section className="instructions">
-        <h2>Cooking Instructions</h2>
-        {steps.map((step, i) => (
-          <p key={i}>{step}</p>
-        ))}
-      </section>
+      <IngredientList list={ingredients} />
+      <Instructions title="Cooking Instructions" steps={steps} />
     </section>
   );
 }
 
 Recipe.propTypes = {
   name: PropTypes.string,
-  ingredients: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      amount: PropTypes.number,
-      measurement: PropTypes.string,
-    })
-  ),
-  steps: PropTypes.arrayOf(PropTypes.string),
+  ingredients: PropTypes.array,
+  steps: PropTypes.array,
 };
